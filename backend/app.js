8d7b5ff4a2515273a15794f3da52b0958118cbb6
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-01-09 23:38:21 +0530

    Added morgan cors cookieParser and express middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require("express")
 const app = express()
 
+const cookieParser = require("cookie-parser")
+const cors = require("cors") //FIXME:
+const morgan = require("morgan") //FIXME: Winston complex setup
+
+app.use(express.urlencoded({extended: true})) //FIXME:
+app.use(express.json())
+app.use(cors()) //Proper URL or pattern not *
+app.use(cookieParser())
+app.use(morgan("tiny")) //FIXME: Prints info about api req and res. combined(X) tiny(✅)
 
 
 module.exports = app
